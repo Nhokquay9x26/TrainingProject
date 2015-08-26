fc2ccab58c3f2674ae1df4c04847c6aa574e3f4e
Do Phu Quy <quydp@asiantech.vn>
2015-08-26 17:20:24 +0700

    add splash fragment

---
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/activities/InitializeActivity.java
@@ -0,0 +1,34 @@
+package vn.asiantech.LearingEnglish.activities;
+
+import android.support.v4.app.FragmentTransaction;
+
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.FragmentByTag;
+
+import vn.asiantech.LearingEnglish.R;
+import vn.asiantech.LearingEnglish.fragments.SplashFragment;
+import vn.asiantech.LearingEnglish.fragments.SplashFragment_;
+
+/**
+ * Copyright © 2015 AsianTech inc.
+ * Created by PhuQuy on 8/26/15.
+ */
+@EActivity(R.layout.activity_initialize)
+public class InitializeActivity extends BaseActionBarActivity {
+
+    @FragmentByTag ("SplashFragment")
+    protected SplashFragment mSplashFragment;
+
+    @Override
+    void afterView() {
+        if (mSplashFragment == null){
+            mSplashFragment = SplashFragment_.builder().build();
+        }
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.frameLayout, mSplashFragment, "SplashFragment");
+        ft.commit();
+    }
+
+
+}
---
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/fragments/SplashFragment.java
@@ -0,0 +1,20 @@
+package vn.asiantech.LearingEnglish.fragments;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EFragment;
+
+import vn.asiantech.LearingEnglish.R;
+
+/**
+ * Copyright © 2015 AsianTech inc.
+ * Created by PhuQuy on 8/26/15.
+ */
+@EFragment(R.layout.fragment_splash)
+public class SplashFragment extends BaseFragment {
+
+    @AfterViews
+    public void afterViews() {
+
+    }
+
+}
