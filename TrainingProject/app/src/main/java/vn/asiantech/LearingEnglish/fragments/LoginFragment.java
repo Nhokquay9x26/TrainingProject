30d907b3192a9b9ea30bdb907cd916e8e62c1607
nhokquay9x26 <daoquocviet11i3@gmail.com>
2015-08-27 19:23:05 +0700

    update class LoginFragment

--- TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/fragments/LoginFragment.java
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/fragments/LoginFragment.java
@@ -1,9 +1,4 @@
 package vn.asiantech.LearingEnglish.fragments;
-
-import android.app.Activity;
-
-import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.EFragment;
 
 import vn.asiantech.LearingEnglish.R;
@@ -11,7 +6,6 @@ import vn.asiantech.LearingEnglish.R;
 /**
  * Created by nhokquay9x26 on 27/08/2015.
  */
-
 @EFragment(R.layout.fragment_login)
 public class LoginFragment extends BaseFragment {
 
