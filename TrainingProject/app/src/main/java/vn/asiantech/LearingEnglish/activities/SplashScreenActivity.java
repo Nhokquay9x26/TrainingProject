e54f3925af281ebd53a02e615703ba79cae226b2
TanTV <tanht268@gmail.com>
2015-08-28 06:57:15 +0700

    * [Group 2] Done function splash screen

--- TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/activities/SplashScreenActivity.java
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/activities/SplashScreenActivity.java
@@ -1,11 +1,61 @@
 package vn.asiantech.LearingEnglish.activities;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.view.View;
+import android.widget.ImageView;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import vn.asiantech.LearingEnglish.R;
+
 /**
  * Created by tantv on 27/08/2015.
  */
+
+@EActivity(R.layout.activity_splashscreen)
 public class SplashScreenActivity extends BaseActionBarActivity {
+    private Timer mTimer;
+    private Intent mIntent;
+    private AnimationDrawable mMyAnimationDrawable1;
+
+    @ViewById(R.id.imgIncrementingBoxView)
+    ImageView mImgincrementingBoxView;
+
     @Override
     void afterView() {
+        getSupportActionBar().hide();
+        //********* box incrementing ***********
+        mImgincrementingBoxView.setVisibility(View.GONE);
+        //--------------------------------------
+        //Call Method set Timer
+        setTimer();
 
     }
+
+    /**
+     * set Timer Activity Timer = 3s
+     */
+
+    private void setTimer() {
+        mTimer = new Timer();
+        mIntent = new Intent(this, LoginActivity_.class);
+        mImgincrementingBoxView.setVisibility(View.VISIBLE);
+        incrementalHorizontalLoading();
+        mTimer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                startActivity(mIntent);
+            }
+        }, 3000);
+    }
+
+    public void incrementalHorizontalLoading() {
+        mMyAnimationDrawable1 = (AnimationDrawable) mImgincrementingBoxView.getDrawable();
+        mMyAnimationDrawable1.start();
+    }
+
 }
