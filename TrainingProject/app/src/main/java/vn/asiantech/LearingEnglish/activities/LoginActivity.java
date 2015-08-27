02ce810bae1722a3acb9b52d46e3d74e7125517c
xuanphu <xuanphuktvn1@gmail.c>
2015-08-27 15:42:11 +0700

    *[Nhom 2] check info input, show toast

--- TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/activities/LoginActivity.java
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/activities/LoginActivity.java
@@ -1,6 +1,11 @@
 package vn.asiantech.LearingEnglish.activities;
 
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.*;
 
 import vn.asiantech.LearingEnglish.R;
 
@@ -9,9 +14,25 @@ import vn.asiantech.LearingEnglish.R;
  */
 @EActivity(R.layout.activity_login)
 public class LoginActivity extends BaseActionBarActivity{
-
+    public static final String TAG = "ACTIVITY_LOGIN";
+    @ViewById(R.id.edtUsername)
+    EditText edtUsername;
+    @ViewById(R.id.edtPassword)
+    EditText edtPassword;
     @Override
     void afterView() {
-
+    }
+    @Click
+    void btnSignIn(){
+        String username = "admin";
+        String password = "admin";
+        if (edtUsername.getText().toString().equals(username)  && edtPassword.getText().toString().equals(password)){
+            Toast toast = Toast.makeText(this, "thanh Cong ...", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        else {
+            Toast toast = Toast.makeText(this, "User or Pass khong dung ...", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
