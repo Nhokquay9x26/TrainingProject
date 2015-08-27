1da7e2a020fa22e2f7d90fa80213e8d673a043e1
TanTV <tanht268@gmail.com>
2015-08-27 16:21:21 +0700

    + [Group 2] Add file SplashScreenActivity.java and XML activity_splashscreen.xml

---
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/activities/SplashScreenActivity.java
@@ -0,0 +1,11 @@
+package vn.asiantech.LearingEnglish.activities;
+
+/**
+ * Created by tantv on 27/08/2015.
+ */
+public class SplashScreenActivity extends BaseActionBarActivity {
+    @Override
+    void afterView() {
+
+    }
+}
