6a9908577b17f631403d0d1364824c7a5f855d9d
xuanphu <xuanphuktvn1@gmail.c>
2015-08-27 14:10:11 +0700

    [Nhom 2] create new login screen

--- TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/activities/MainActivity.java
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/activities/MainActivity.java
@@ -1,14 +1,11 @@
 package vn.asiantech.LearingEnglish.activities;
 
-import android.widget.Button;
-import android.widget.TextView;
+
 
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.OptionsItem;
-import org.androidannotations.annotations.*;
 
 import vn.asiantech.LearingEnglish.R;
-
 /**
  * Created by tientun on 3/5/15.
  */
