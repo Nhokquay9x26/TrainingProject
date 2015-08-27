b91cf97ab6f745a1df72f5859c3946b704b14292
nhokquay9x26 <daoquocviet11i3@gmail.com>
2015-08-27 17:19:59 +0700

    create new class LoginFragment

---
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/fragments/LoginFragment.java
@@ -0,0 +1,18 @@
+package vn.asiantech.LearingEnglish.fragments;
+
+import android.app.Activity;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.EFragment;
+
+import vn.asiantech.LearingEnglish.R;
+
+/**
+ * Created by nhokquay9x26 on 27/08/2015.
+ */
+
+@EFragment(R.layout.fragment_login)
+public class LoginFragment extends BaseFragment {
+
+}
