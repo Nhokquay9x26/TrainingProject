a24292a45971467cda47cd26c1294236ea3c8cec
conall33 <conall33@gmail.com>
2015-08-28 16:35:24 +0700

    new fragment signup (version 1)

---
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/core/fragments/SignupFragment.java
@@ -0,0 +1,13 @@
+package vn.asiantech.LearingEnglish.core.fragments;
+
+import android.support.v4.app.Fragment;
+
+import org.androidannotations.annotations.EFragment;
+
+import vn.asiantech.LearingEnglish.R;
+
+@EFragment(R.layout.fragment_signup)
+public class SignupFragment extends Fragment {
+
+
+}
