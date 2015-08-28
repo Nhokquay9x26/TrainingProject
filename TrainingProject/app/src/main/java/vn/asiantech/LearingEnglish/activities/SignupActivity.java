1701984494504d4175b47edab6d4069b48b7a6cc
mrson <thaisonm360@gmail.com>
2015-08-28 12:52:20 +0700

    creat design UI

---
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/activities/SignupActivity.java
@@ -0,0 +1,16 @@
+package vn.asiantech.LearingEnglish.activities;
+
+import org.androidannotations.annotations.EActivity;
+
+import vn.asiantech.LearingEnglish.R;
+
+/**
+ * Created by mrson on 28/08/2015.
+ */
+@EActivity (R.layout.activity_signup)
+public class SignupActivity extends BaseActionBarActivity{
+    @Override
+    void afterView() {
+
+    }
+}
