476c61cd4968d5b5321ca05ddc3ed2479e2d0648
mrson <thaisonm360@gmail.com>
2015-08-28 15:50:41 +0700

    edit signup event

--- TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/activities/SignupActivity.java
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/activities/SignupActivity.java
@@ -1,6 +1,11 @@
 package vn.asiantech.LearingEnglish.activities;
 
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 import vn.asiantech.LearingEnglish.R;
 
@@ -9,8 +14,24 @@ import vn.asiantech.LearingEnglish.R;
  */
 @EActivity (R.layout.activity_signup)
 public class SignupActivity extends BaseActionBarActivity{
+    @ViewById (R.id.edt_username)
+    EditText mEdtUsername;
+    @ViewById (R.id.edt_email)
+    EditText mEdtEmail;
+    @ViewById (R.id.edtPassword)
+    EditText mEdtPassword;
+    @ViewById (R.id.edtConfirm_pass)
+    EditText mEdtConfirmPass;
+    @ViewById (R.id.edtPromotion)
+    EditText mEdtPromotion;
+
     @Override
     void afterView() {
 
     }
+    @Click (R.id.tvButton_signUp)
+    void eventSignUp(){
+        Toast.makeText(this,"Test",Toast.LENGTH_LONG).show();
+    }
+
 }
