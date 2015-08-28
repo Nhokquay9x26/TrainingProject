f102168b27db37b846717fb6fb76651b213324fd
mrson <thaisonm360@gmail.com>
2015-08-28 10:11:14 +0700

    fix conflig

--- TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/activities/LoginActivity.java
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/activities/LoginActivity.java
@@ -12,6 +12,6 @@ public class LoginActivity extends BaseActionBarActivity{
 
     @Override
     void afterView() {
-
+        getSupportActionBar().hide();
     }
 }
--- TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/activities/SplashScreenActivity.java
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/activities/SplashScreenActivity.java
@@ -1,7 +1,14 @@
 package vn.asiantech.LearingEnglish.activities;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.view.View;
+import android.widget.ImageView;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import vn.asiantech.LearingEnglish.R;
 
 /**
@@ -10,9 +17,44 @@ import vn.asiantech.LearingEnglish.R;
 
 @EActivity(R.layout.activity_splashscreen)
 public class SplashScreenActivity extends BaseActionBarActivity {
+    private Timer mTimer;
+    private Intent mIntent;
+    private AnimationDrawable mMyAnimationDrawable1;
+
+    @ViewById(R.id.imgIncrementingBoxView)
+    ImageView mImgincrementingBoxView;
 
     @Override
     void afterView() {
+        getSupportActionBar().hide();
+        //********* box incrementing ***********
+        mImgincrementingBoxView.setVisibility(View.GONE);
+        //--------------------------------------
+        //Call Method set Timer
+        setTimer();
 
+    }
+
+    /**
+     * set Timer Activity Timer = 3s
+     */
+
+    private void setTimer() {
+        mTimer = new Timer();
+        mIntent = new Intent(this, LoginActivity_.class);
+        mImgincrementingBoxView.setVisibility(View.VISIBLE);
+        incrementalHorizontalLoading();
+        mTimer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                startActivity(mIntent);
+            }
+        }, 3000);
+    }
+
+    public void incrementalHorizontalLoading() {
+        mMyAnimationDrawable1 = (AnimationDrawable) mImgincrementingBoxView.getDrawable();
+        mMyAnimationDrawable1.start();
     }
 }
