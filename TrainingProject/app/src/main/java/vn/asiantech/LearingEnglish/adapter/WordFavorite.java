63879bb0c75aafda2d599e76c0097230275cdf30
HoaiDVV <HoaiDVV@asiantech.vn>
2015-09-01 10:09:16 +0700

    create xml, adapter, model for favorite tab

---
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/adapter/WordFavorite.java
@@ -0,0 +1,83 @@
+package vn.asiantech.LearingEnglish.adapter;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import vn.asiantech.LearingEnglish.R;
+
+/**
+ * Created by hoai on 31/08/2015.
+ */
+public class WordFavorite extends RecyclerView.Adapter<WordFavorite.RecyclerViewHolder> {
+
+    private List<vn.asiantech.LearingEnglish.models.WordFavorite> mFavorites = new ArrayList<>();
+    private Context mContext;
+    public WordFavorite(List<vn.asiantech.LearingEnglish.models.WordFavorite> mFavorites, Context context) {
+        this.mFavorites = mFavorites;
+        this.mContext = context;
+    }
+
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup viewGroup, int positon) {
+        View mViewItem = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_list_favorite, viewGroup, false);
+        return new RecyclerViewHolder(mViewItem);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder viewHolder, int position) {
+        viewHolder.mTvWord.setText(mFavorites.get(position).getWord());
+        viewHolder.mTvPronunciation.setText(mFavorites.get(position).getPronunciation());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFavorites.size();
+    }
+
+    /**
+     * Viewholder for item view of list
+     */
+   class RecyclerViewHolder extends RecyclerView.ViewHolder implements TextToSpeech.OnInitListener, View.OnClickListener {
+        private TextView mTvWord, mTvPronunciation;
+        private ImageView mImgSound;
+        private TextToSpeech mTextToSpeed;
+
+        public RecyclerViewHolder(View convertView) {
+            super(convertView);
+            mTextToSpeed = new TextToSpeech(mContext, this);
+            mTvWord = (TextView) convertView.findViewById(R.id.tvWordFavorite);
+            mTvPronunciation = (TextView) convertView.findViewById(R.id.tvSpellWord);
+            mImgSound = (ImageView) convertView.findViewById(R.id.imgSound);
+            mImgSound.setOnClickListener(this);
+
+        }
+
+        private void excuteSound(String word){
+            mTextToSpeed.speak(word,TextToSpeech.QUEUE_FLUSH,null);
+        }
+
+        @Override
+        public void onInit(int status) {
+            if(status!= TextToSpeech.ERROR){
+                mTextToSpeed.setLanguage(Locale.US);
+            }
+        }
+
+        @Override
+        public void onClick(View view) {
+            excuteSound(mTvWord.getText().toString());
+        }
+    }
+}
+
