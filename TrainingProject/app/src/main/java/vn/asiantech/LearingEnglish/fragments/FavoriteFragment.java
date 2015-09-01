8086f2a7f73a382299b3878b31e10262f4ee9ce6
mrson <thaisonm360@gmail.com>
2015-09-01 10:04:13 +0700

    new fragment favorite

---
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/fragments/FavoriteFragment.java
@@ -0,0 +1,14 @@
+package vn.asiantech.LearingEnglish.fragments;
+
+import org.androidannotations.annotations.EFragment;
+
+import vn.asiantech.LearingEnglish.R;
+
+/**
+ * @author mrs
+ * Created by mrson on 31/08/2015.
+ */
+@EFragment (R.layout.fragment_favorite)
+public class FavoriteFragment extends BaseFragment {
+
+}
