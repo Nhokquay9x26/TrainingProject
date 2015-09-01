131740fe857e1f3e1fd512ee4f5455f06ba13cbb
Vo Chi Thanh <vochithanh1989>
2015-09-01 10:08:58 +0700

    Update Activity LognIn, UI SignIn, add image

--- TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/activities/LoginActivity.java
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/activities/LoginActivity.java
@@ -2,6 +2,7 @@ package vn.asiantech.LearingEnglish.activities;
 
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.androidannotations.annotations.Click;
@@ -9,13 +10,22 @@ import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
 import vn.asiantech.LearingEnglish.R;
+import vn.asiantech.LearingEnglish.utils.Utils;
 
 /**
  * Created by thanhitbk on 31/08/2015.
  */
 @EActivity(R.layout.activity_login)
 public class LoginActivity extends BaseActionBarActivity {
-    final static String MESSAGE_ERROR_LOGIN = "Please type your username/email and password!";
+    final static String MESSAGE_EMPTY_LOGIN = "Please type your username/email and password!";
+    final static String MESSAGE_ERROR_LOGIN = "username or password is incorrect";
+
+    @ViewById(R.id.tvForgotPassWord)
+    TextView mTvForgotPassWord;
+
+    @ViewById(R.id.btnSignUp)
+    Button mBtnSignUp;
+
     @ViewById(R.id.btnSignIn)
     Button mBtnSignIn;
 
@@ -34,11 +44,26 @@ public class LoginActivity extends BaseActionBarActivity {
         final String email = mEdtUserName.getText().toString();
         final String password = mEdtPassword.getText().toString();
         if (email.length() > 0 && password.length() > 0) {
-            MainActivity_.intent(LoginActivity.this).start();
-            finish();
+            if (email == "asiantech@asiantech.vn" && password == "123456") {
+                MainActivity_.intent(LoginActivity.this).start();
+                finish();
+            } else {
+                Toast.makeText(getApplicationContext(), MESSAGE_ERROR_LOGIN, Toast.LENGTH_LONG).show();
+            }
         } else {
-            Toast.makeText(getApplicationContext(), MESSAGE_ERROR_LOGIN, Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), MESSAGE_EMPTY_LOGIN, Toast.LENGTH_LONG).show();
         }
 
+    }
+
+    @Click(R.id.btnSignUp)
+    void clickSignUp() {
+        SignUpActivity_.intent(LoginActivity.this).start();
+        finish();
+    }
+
+    @Click(R.id.tvForgotPassWord)
+    void clickForgotPassword() {
+        Utils.callDialogForgetPass(this);
     }
 }
