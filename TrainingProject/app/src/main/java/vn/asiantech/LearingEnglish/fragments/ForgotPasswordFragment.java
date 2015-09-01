52981e3b88c5d35d50c59eadc7908662982aa19c
asiantechnguyentrinh <nguyentrinh11t@gmail.com>
2015-09-01 16:56:52 +0700

    add ForgotPasswordFragment,edit ic_forgot_pass,fragment_forgot_password

---
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/fragments/ForgotPasswordFragment.java
@@ -0,0 +1,16 @@
+package vn.asiantech.LearingEnglish.fragments;
+
+import org.androidannotations.annotations.EFragment;
+
+import vn.asiantech.LearingEnglish.R;
+
+/**
+ * Created by nguyentrinh on 01/09/2015.
+ */
+@EFragment(R.layout.fragment_forgot_password)
+public class ForgotPasswordFragment extends BaseFragment {
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
