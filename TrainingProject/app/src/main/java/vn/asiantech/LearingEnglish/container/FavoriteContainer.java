c8e59c1472c5288a54a1eca5ff40b2d9c79384b3
HuongNV <HuongNV@asiantech.vn>
2015-09-01 17:22:58 +0700

    update code

---
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/container/FavoriteContainer.java
@@ -0,0 +1,42 @@
+package vn.asiantech.LearingEnglish.container;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.androidannotations.annotations.EFragment;
+
+import vn.asiantech.LearingEnglish.R;
+import vn.asiantech.LearingEnglish.fragments.BaseContainerFragment;
+import vn.asiantech.LearingEnglish.fragments.FavoriteFragment;
+
+
+public class FavoriteContainer extends BaseContainerFragment {
+
+    private boolean mIsViewInited;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return View.inflate(getActivity(), R.layout.container_fragment, null);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        if (!mIsViewInited) {
+            mIsViewInited = true;
+            initView();
+        }
+    }
+
+    private void initView() {
+        replaceFragment(new FavoriteFragment(), false);
+    }
+
+    @Override
+    public Fragment getChildFragment() {
+        return getChildFragmentManager().findFragmentById(R.id.container_framelayout);
+    }
+}
---
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/models/WordFavorite.java
@@ -0,0 +1,28 @@
+package vn.asiantech.LearingEnglish.models;
+
+
+
+/**
+ * Created by hoai on 31/08/2015.
+ */
+
+public class WordFavorite {
+    private String mWord;
+    private String mPronunciation;
+
+    public String getWord() {
+        return mWord;
+    }
+
+    public void setWord(String mWord) {
+        this.mWord = mWord;
+    }
+
+    public String getPronunciation() {
+        return mPronunciation;
+    }
+
+    public void setPronunciation(String mPronunciation) {
+        this.mPronunciation = mPronunciation;
+    }
+}
