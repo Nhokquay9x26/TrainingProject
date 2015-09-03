4191153cd89210def85fd57377bd6ce8126b2971
nhokquay9x26 <daoquocviet11i3@gmail.com>
2015-09-03 16:34:44 +0700

    create LoginFragment, fix convention fragment_login, fragment_note and values

---
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/fragments/LoginFragment.java
@@ -0,0 +1,19 @@
+package vn.asiantech.LearingEnglish.fragments;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.EFragment;
+
+import vn.asiantech.LearingEnglish.R;
+
+/**
+ * Created by nhokquay9x26 on 02/09/2015.
+ */
+@EFragment(R.layout.fragment_login)
+public class LoginFragment extends BaseFragment{
+
+    @AfterViews
+    public void init(){
+
+    }
+}
