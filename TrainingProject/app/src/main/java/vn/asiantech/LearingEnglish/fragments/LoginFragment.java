f3c8acff4811dddef2a114e1b581ea246a5fd69c
nhokquay9x26 <daoquocviet11i3@gmail.com>
2015-09-04 14:50:49 +0700

    fix author class LoginFragment

--- TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/fragments/LoginFragment.java
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/fragments/LoginFragment.java
@@ -1,17 +1,16 @@
 package vn.asiantech.LearingEnglish.fragments;
 
 import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.EFragment;
 
 import vn.asiantech.LearingEnglish.R;
 
 /**
+ * @author DaoQuocViet
  * Created by nhokquay9x26 on 02/09/2015.
  */
 @EFragment(R.layout.fragment_login)
 public class LoginFragment extends BaseFragment{
-
     @AfterViews
     public void init(){
 
