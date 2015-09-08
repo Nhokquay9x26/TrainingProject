ee18c3efe5193585fc0c71ccf644e46308300b20
Vo Chi Thanh <vochithanh1989>
2015-09-08 08:40:51 +0700

    Update base fragment

--- TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/fragments/BaseFragment.java
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/fragments/BaseFragment.java
@@ -1,8 +1,21 @@
 package vn.asiantech.LearingEnglish.fragments;
 
+import android.app.Activity;
+
+import vn.asiantech.LearingEnglish.container.SettingContainer;
 import vn.asiantech.LearingEnglish.core.fragments.Fragment;
 
 public abstract class BaseFragment extends Fragment {
 
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
 
+    public void replaceFragment(Fragment fragment){
+        android.support.v4.app.Fragment fragmentFarent = getParentFragment();
+        if(fragmentFarent instanceof SettingContainer){
+            ((SettingContainer) fragmentFarent).replaceFragment(fragment, false);
+        }
+    }
 }
