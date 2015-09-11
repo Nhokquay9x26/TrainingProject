480b9bebc99b4780b5c68e6d1b50b91f99fbc331
Tran Quoc Hoan <tranquochoanz@gmail.com>
2015-09-11 10:28:12 +0700

    add API Forgot password

--- TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/network/Api.java
+++ TrainingProject/app/src/main/java/vn/asiantech/LearingEnglish/network/Api.java
@@ -6,6 +6,7 @@ import retrofit.http.FormUrlEncoded;
 import retrofit.http.POST;
 import retrofit.http.Query;
 import vn.asiantech.LearingEnglish.models.BaseModel;
+import vn.asiantech.LearingEnglish.models.ForgotPass;
 import vn.asiantech.LearingEnglish.models.InfoRegister;
 import vn.asiantech.LearingEnglish.models.Login;
 import vn.asiantech.LearingEnglish.network.core.Callback;
@@ -33,5 +34,9 @@ public interface Api {
                   @Field(Parameter.PASSWORD) String password,
                   @Field(Parameter.NAME) String name,
                   Callback<InfoRegister> callback);
+
+    @FormUrlEncoded
+    @POST("/forget_pass")
+    void forgotPassWord(@Field(Parameter.EMAIL) String mEmail, Callback<ForgotPass> mCallback);
 
 }
